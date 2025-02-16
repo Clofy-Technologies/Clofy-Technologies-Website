33ccdccd14d7501d43120f889e365e58f765ffb5
Thillai <thillai@clofy.tech>
2025-02-16 19:16:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
 import { defineConfig } from 'vite';
-
 import react from '@vitejs/plugin-react';
  
 // Vite configuration
-
 export default defineConfig({
-
   plugins: [react()],
-
   optimizeDeps: {
-
     exclude: ['lucide-react'], // Exclude dependencies from optimization
-
   },
-
   build: {
-
-    outDir: 'dist', // Ensure the build output is placed in the root directory's dist folder
-
+    outDir: 'build', // Ensure the build output matches the Azure deployment folder
   },
-
 });
+ 
